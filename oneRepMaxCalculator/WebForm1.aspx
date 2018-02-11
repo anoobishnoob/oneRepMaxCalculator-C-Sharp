4656b03349d4f0fcd9ce9ccf921e348812f30ac3
Samme Qandil <Saqandil@gmail.com>
2018-02-10 22:58:17 -0800

    Add files via upload

---
+++ oneRepMaxCalculator/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="oneRepMaxCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <br />
+            How much weight lifted? (lbs)
+            <asp:TextBox ID="weightTextBox" runat="server"></asp:TextBox>
+&nbsp;<br />
+            How many reps?
+            <asp:TextBox ID="repsTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="calcButton" runat="server" OnClick="calcButton_Click" Text="Go!" />
+            <br />
+            <br />
+            <br />
+            How much is your projected max?<br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ oneRepMaxCalculator/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oneRepMaxCalculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void calcButton_Click(object sender, EventArgs e)
+        {
+            //double value = double.Parse(moutTextBox.Text);
+            int repValue = int.Parse(repsTextBox.Text);
+            int weightValue = int.Parse(weightTextBox.Text);
+
+            int oneRepMax = repValue * weightValue / 30 + weightValue;
+
+            //resultLabel.Text += repValue.ToString(); just did this to check that it is storing the values
+            //resultLabel.Text += weightValue.ToString(); And did not want to set a break point for this
+
+            resultLabel.Text = oneRepMax.ToString();
+        }
+        //one rep max is 
+        //reps * weight/ 30 + weight
+    }
+}
